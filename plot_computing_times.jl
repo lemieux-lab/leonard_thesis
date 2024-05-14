a0fd2dedf2396f249e990e3407657cdfe2a6d701
sauves <leonardsauve@gmail.com>
2024-05-14 00:58:20 -0400

    some extra stats

--- plot_computing_times.jl
+++ plot_computing_times.jl
@@ -9,12 +9,14 @@ to_files = readdir(basepath)
 OUTFILE = DataFrame()
 datas = []
 repn = []
+losses = []
 for (i, infile) in enumerate(to_files)
-    data  = CSV.read("$basepath/$infile", DataFrame)[:,3]
-    push!(datas, data)
-    push!(repn, ones(length(data)) * i)
+    data  = CSV.read("$basepath/$infile", DataFrame)
+    push!(datas, data[:,3])
+    push!(repn, ones(length(data[:,3])) * i)
+    push!(losses, data[:,2])
 end 
-OUTFILE = DataFrame(:t_second=>vcat(datas...), :repn=>Int64.(vcat(repn...)), :method=>fill("Julia", length(vcat(repn...))) )
+OUTFILE = DataFrame(:t_second=>vcat(datas...), :repn=>Int64.(vcat(repn...)), :lossv=>Float64.(vcat(losses...)), :method=>fill("Julia", length(vcat(repn...))))
 CSV.write("interactive_figures/computing_times_Julia2024.csv",OUTFILE)
 
 JuliaData = CSV.read("interactive_figures/computing_times_Julia2024.csv", DataFrame)
@@ -28,8 +30,19 @@ PythonData = PythonData[PythonData.repn .== 5, :]
 PythonData[:,"stepn"] .= collect(1:size(PythonData)[1])
 PythonData[:,"speed"] .= 1 ./ ( PythonData.t_second .- vcat(PythonData.t_second[1], PythonData.t_second[1:end-1]))
 PythonData = PythonData[2:end,:]
-JuliaData
-PythonData
+median(JuliaData.speed)
+median(PythonData.speed)
+JuliaData.speed
+tmp_data = JuliaData[end-10_000:end, "lossv"]
+X = collect(1:size(tmp_data)[1])
+PearsonR = my_cor(X, tmp_data)
+std_data = std(tmp_data)
+fig = Figure(size = (512,256));
+ax = Axis(fig[1,1], title = "Last 10,000 steps of training\nPearson Correlation : $(round(PearsonR, digits = 4)) \n Standard deviation : $(round(std_data, digits = 8))", xlabel = "steps", ylabel = "loss value")
+scatter!(ax, X, tmp_data,color= RGBAf(0,0,0,0), strokewidth=1,  strokecolor = :black,)
+# hexbin!(ax, X, tmp_data, cellsize = 5)
+fig
+PythonData.speed
 merged_data = vcat(JuliaData, PythonData, cols =:union)
 # plot Python data 
 fig = Figure(size = (1024,512));
