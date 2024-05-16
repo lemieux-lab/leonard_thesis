556f7bddeac2fe53bd0044d9a64d70faa637ab06
sauves <leonardsauve@gmail.com>
2024-05-16 01:39:56 -0400

    interpolation works

--- runs/run_FE_2D_patient_embed_computing_times_learning_curves.jl
+++ runs/run_FE_2D_patient_embed_computing_times_learning_curves.jl
@@ -17,13 +17,13 @@ generate_params(X_data) = return Dict(
     ## run infos 
     "session_id" => session_id,  "modelid" =>  "$(bytes2hex(sha256("$(now())"))[1:Int(floor(end/3))])",
     "outpath"=>outpath, "machine_id"=>strip(read(`hostname`, String)), "device" => "$(device())",
-    "printstep"=>1000, 
+    "printstep"=>10_000, 
     ## data infos 
     "nsamples" =>size(X_data)[1], "ngenes"=> size(X_data)[2],  
     ## optim infos 
-    "lr" => 1e-2, "l2" => 1e-7,"nsteps" => 10_000, "nsteps_inference" => 10_000, "batchsize" => 40_000,
+    "lr" => 1e-2, "l2" => 1e-7,"nsteps" => 20_000, "nsteps_inference" => 10_000, "batchsize" => 40_000,
     ## model infos
-    "emb_size_1" => 2, "emb_size_2" => 50, "fe_layers_size"=> [250,75,50,25,10]#, "fe_hl1_size" => 50, "fe_hl2_size" => 50,
+    "emb_size_1" => 2, "emb_size_2" => 50, "fe_layers_size"=> [25]#, "fe_hl1_size" => 50, "fe_hl2_size" => 50,
     )
 # train whole dataset 
 # params = generate_params(X_data)
@@ -49,3 +49,32 @@ CairoMakie.save("$(params["outpath"])/$(params["modelid"])_2D_embedding_train_te
 
 ## PlotlyJS
 plot_interactive(params, cpu(trained_FE.net[1][1].weight), cpu(test_model[1][1].weight), train_ids, test_ids,  labels)
+
+sample_profile = test_data[250,:]
+grid = collect(-25:1:25) ./ 10
+grid_vals = zeros(size(grid)[1] , size(grid)[1])
+gene_embed = trained_FE.net[1][2](gpu(collect(1:size(genes[CDS])[1])))
+Xs = []
+Ys = []
+for i in 1:size(grid)[1]
+    println(i)
+    for j in 1:size(grid)[1]
+        grid_val = vcat(gpu(ones(size(genes[CDS])[1]) * grid[i])', gpu(ones(size(genes[CDS])[1]) * grid[j])', gene_embed)
+        
+        infer_profile = vec(trained_FE.net[2:end](grid_val))
+        grid_vals[i,j] =  cpu(my_cor(gpu(sample_profile), infer_profile))
+        push!(Xs, grid[i])
+        push!(Ys, grid[j])
+    end 
+end 
+fig = Figure(size = (512,512));
+ax = Axis(fig[1,1], title = "Heatmap", xlabel = "EMBED-1", ylabel = "EMBED-2" )
+scatter!(ax, Float64.(Xs), Float64.(Ys) , color=vec(grid_vals), colormap=:viridis)
+# fig[1,2] = Colorbar(scat)
+Xs[vec(grid_vals) .== maximum(vec(grid_vals))]
+Ys[vec(grid_vals) .== maximum(vec(grid_vals))]
+# Xs[vec(grid_vals) .== maximum(vec(grid_vals))]
+# Ys[vec(grid_vals) .== maximum(vec(grid_vals))]
+# grid_vals[]
+# grid_vals
+fig
