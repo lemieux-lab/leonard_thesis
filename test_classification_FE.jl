5f034113855f9c189a302b378c89413377133d0c
sauves <leonardsauve@gmail.com>
2024-05-28 02:12:46 -0400

    added files

---
+++ test_classification_FE.jl
@@ -0,0 +1,94 @@
+include("engines/init.jl")
+include("engines/factorized_embeddings.jl")
+include("engines/figures.jl")
+include("engines/data_processing.jl")
+include("engines/utils.jl")
+outpath, session_id = set_dirs("FE_RES")
+TCGA_data, labs, patients, genes, biotypes = load_tcga_dataset("Data/GDC_processed/TCGA_TPM_lab.h5")
+labels = annotate_labels(labs, "Data/GDC_processed/TCGA_abbrev.txt")
+CDS = biotypes .== "protein_coding"
+# X_data = TCGA_data[:,CDS]
+X_data = TCGA_data[:,CDS]
+# split train / test 
+folds = split_train_test(X_data, nfolds = 5) # split 80-20
+train_ids, train_data, test_ids, test_data = folds[1]["train_ids"], folds[1]["train_x"], folds[1]["test_ids"], folds[1]["test_x"]
+# set params 
+generate_params(X_data) = return Dict( 
+    ## run infos 
+    "session_id" => session_id,  "modelid" =>  "$(bytes2hex(sha256("$(now())"))[1:Int(floor(end/3))])",
+    "outpath"=>outpath, "machine_id"=>strip(read(`hostname`, String)), "device" => "$(device())",
+    "printstep"=>10_000, 
+    ## data infos 
+    "nsamples" =>size(X_data)[1], "ngenes"=> size(X_data)[2],  
+    ## optim infos 
+    "lr" => 1e-2, "l2" => 1e-7,"nsteps" => 100_000, "nsteps_inference" => 10_000, "batchsize" => 40_000,
+    ## model infos
+    "emb_size_1" => 125, "emb_size_2" => 50, "fe_layers_size"=> [250, 75, 50, 25, 10]#, "fe_hl1_size" => 50, "fe_hl2_size" => 50,
+    )
+# train whole dataset 
+# params = generate_params(X_data)
+# trained_FE,  tr_epochs , tr_loss, tr_cor =  generate_patient_embedding(X_data, patients, genes[CDS], params, labels)
+# continue training on embedding layer only.
+# tr_epochs , tr_loss, tr_cor =  train_embed_SGD!(params, X,Y, trained_FE)
+
+# train with training set
+params = generate_params(X_data)
+# save IDs
+# bson("$(params["outpath"])/$(params["modelid"])_train_test_ids.bson", 
+#     Dict("train_ids"=> train_ids, "test_ids"=>test_ids, 
+#     "model_prefix"=> "$(params["outpath"])/$(params["modelid"])"))
+trained_FE,  tr_epochs , tr_loss, tr_cor =  generate_patient_embedding(X_data, patients, genes[CDS], params, labels)
+patient_FE = cpu(trained_FE.net[1][1].weight)
+DF = DataFrame(Dict([("EMBED$(i)", patient_FE[i,:]) for i in 1:size(patient_FE)[1]]))
+CSV.write("$(params["outpath"])/$(params["modelid"])_patient_embed.csv", DF)
+
+function test_classification_perf(X_data, labels;nsteps=2000)
+    dimFE = size(X_data)[1] 
+    npatients = size(X_data)[2]
+    outsize = length(unique(labels))
+    Y = zeros( npatients, outsize)
+    unique_labs = unique(labels)
+    [Y[i, unique_labs .== lab] .= 1 for (i,lab) in enumerate(labels)]
+    Y_data_clf = gpu(Matrix(Y'))
+    folds = split_train_test(Matrix(X_data'), nfolds = 5) # split 80-20
+    outfile = "modelid\tfoldn\tacc\t"
+    for (foldn, fold) in enumerate(folds)
+    train_ids, train_x, test_ids, test_x = fold["train_ids"], gpu(Matrix(fold["train_x"])'), fold["test_ids"], gpu(Matrix(fold["test_x"]'))
+    train_y = gpu(Matrix(Y')[:,train_ids])
+    test_y = gpu(Matrix(Y')[:,test_ids])
+    model = gpu(Flux.Chain(Dense(size(train_x)[1], 250, leakyrelu), Dense(250, 100, leakyrelu),
+        Dense(100, 50, leakyrelu), Dense(50, outsize), softmax
+        ))
+    opt = Flux.setup(OptimiserChain(Flux.WeightDecay(1e-6), Flux.Optimise.Adam(0.0001)), model);
+    for i in 1:nsteps
+        grads = Flux.gradient(model) do m
+            loss = sum(Flux.crossentropy(m(train_x), train_y))
+        end 
+        outs = model(train_x)
+        tr_lossval = sum(Flux.crossentropy(outs, train_y))
+        tr_acc = sum((maximum(outs, dims = 1) .== outs)' .& (Int.(train_y))') / size(train_x)[2]
+        
+        tst_outs = model(test_x)
+        tst_lossval = Flux.mse(tst_outs, test_y)
+        tst_acc = sum((maximum(tst_outs, dims = 1) .== tst_outs)' .& (Int.(test_y))') / size(test_x)[2]
+        
+        Flux.update!(opt, model, grads[1])
+        if i % 100 == 0
+            println("FOLDN $foldn - STEP $i - TRAIN : loss : $(tr_lossval) acc: $(tr_acc)\n TEST: loss: $(tst_lossval) acc: $(tst_acc)")
+        end
+    end
+    tst_outs = model(test_x)
+    ACC =  sum((maximum(tst_outs, dims = 1) .== tst_outs)' .& (Int.(test_y))') / size(test_x)[2]
+    
+    outfile = "$outfile\n$(params["modelid"])\t$foldn\t$ACC\t"
+    end 
+    return outfile
+end 
+FE_outfile = test_classification_perf(patient_FE, labels)
+println(outfile)
+size(patient_FE)
+size(X_data)
+
+full_profile_outfile = test_classification_perf(Matrix(X_data'), labels)
+
+println(full_profile_outfile)
