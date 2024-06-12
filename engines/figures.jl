7aba95f0578539f7498597c0c99d0232c0ac4486
sauves <leonardsauve@gmail.com>
2024-06-12 18:17:48 -0400

    patient embed by sample working..

--- engines/figures.jl
+++ engines/figures.jl
@@ -81,7 +81,20 @@ function plot_FE_reconstruction(model, X, Y;modelID="")
     #resize_to_layout!(fig)
     return fig
 end 
-
+function output_labels_legend(params)
+    fig = Figure(size = (600,900));
+    ax = Axis(fig[1,1]);
+    colors_labels_df = CSV.read(params["colorsFile"], DataFrame)
+    for (i, group_lab) in enumerate(unique(labs))
+        group = labs .== group_lab
+        col = colors_labels_df[colors_labels_df[:,"labs"] .== group_lab,"hexcolor"][1]
+        name = colors_labels_df[colors_labels_df[:,"labs"] .== group_lab,"name"][1]
+        scatter!(ax,[1],[1], strokewidth = 0.1, color = String(col), label = name)
+    end
+    fig[1,2] = axislegend(ax, position = :rc)
+    fig
+    CairoMakie.save("figures/TCGA_cancer_types_colors.pdf", fig)
+end 
 
 function generate_TGCA_colors_dict(labs)
     tcga_abbrv = CSV.read("Data/GDC_processed/TCGA_abbrev.txt", DataFrame)
--- runs/run_FE_2D_by_step_comp_times_learning_curves_scatter_embed_cancer_type.jl
+++ runs/run_FE_2D_by_step_comp_times_learning_curves_scatter_embed_cancer_type.jl
@@ -3,8 +3,6 @@ include("engines/factorized_embeddings.jl")
 include("engines/figures.jl")
 include("engines/data_processing.jl")
 include("engines/utils.jl")
-include("engines/gpu_utils.jl")
-device!()
 outpath, session_id = set_dirs("FE_RES")
 TCGA_data, labs, patients, genes, biotypes = load_tcga_dataset("Data/GDC_processed/TCGA_TPM_lab.h5")
 CDS = biotypes .== "protein_coding"
@@ -14,7 +12,7 @@ X_data = TCGA_data[:,CDS]
 folds = split_train_test(X_data, nfolds = 5) # split 80-20
 train_ids, train_data, test_ids, test_data = folds[1]["train_ids"], folds[1]["train_x"], folds[1]["test_ids"], folds[1]["test_x"]
 # set params 
-generate_params(X_data;nsamples_batchsize=1) = return Dict( 
+generate_params(X_data;nsamples_batchsize=4) = return Dict( 
     ## run infos 
     "session_id" => session_id,  "modelid" =>  "$(bytes2hex(sha256("$(now())"))[1:Int(floor(end/3))])",
     "outpath"=>outpath, "machine_id"=>strip(read(`hostname`, String)), "device" => "$(device())",
@@ -24,9 +22,9 @@ generate_params(X_data;nsamples_batchsize=1) = return Dict(
     "nsamples" =>size(X_data)[1], "ngenes"=> size(X_data)[2],  
     "nsamples_batchsize"=> nsamples_batchsize, 
     ## optim infos 
-    "lr" => 1e-2, "l2" => 1e-7,"nsteps" => 100_000, "nsteps_inference" => 10_000, "batchsize" => size(X_data)[2] * nsamples_batchsize,
+    "lr" => 1e-2, "l2" => 0,"nsteps" => 100_000, "nsteps_inference" => 10_000, "batchsize" => nsamples_batchsize * size(X_data)[2],
     ## model infos
-    "emb_size_1" => 2, "emb_size_2" => 50, "fe_layers_size"=> [150,100,50]#, "fe_hl1_size" => 50, "fe_hl2_size" => 50,
+    "emb_size_1" => 2, "emb_size_2" => 100, "fe_layers_size"=> [100,50,50]#, "fe_hl1_size" => 50, "fe_hl2_size" => 50,
     )
 # train whole dataset 
 # params = generate_params(X_data)
@@ -34,23 +32,116 @@ generate_params(X_data;nsamples_batchsize=1) = return Dict(
 # continue training on embedding layer only.
 # tr_epochs , tr_loss, tr_cor =  train_embed_SGD!(params, X,Y, trained_FE)
 
-model = FE_model(params)
-model.net[1]
 # train with training set
-params = generate_params(X_data, nsamples_batchsize=4)
+params = generate_params(X_data, nsamples_batchsize = 10)
 # save IDs
 # bson("$(params["outpath"])/$(params["modelid"])_train_test_ids.bson", 
 #     Dict("train_ids"=> train_ids, "test_ids"=>test_ids, 
 #     "model_prefix"=> "$(params["outpath"])/$(params["modelid"])"))
-trained_FE,  tr_epochs , tr_loss, tr_cor =  generate_patient_embedding(X_data, patients, genes[CDS], params, labs)
-inference_model = do_inference(trained_FE.net, params,X_data,patients,genes[CDS])
 
-trained_patient_FE = cpu(inference_model[1][1].weight)
+trained_FE,  tr_epochs , tr_loss, tr_cor =  generate_patient_embedding(X_data, patients, genes[CDS], params, labs)
+X, Y = prep_FE(X_data, patients, genes[CDS], order = "per_sample");
+
+# Option 1 : quickest TOO SLOW 
+# by patient in training set
+new_embed = zeros((params["nsamples"], 2));
+nsamples_batchsize = 200
+batchsize = params["ngenes"] * nsamples_batchsize
+nminibatches = Int(floor(params["nsamples"] / nsamples_batchsize))
+
+for target_id in 1:params["nsamples"]
+    #generate imputed profile
+    target_range = (target_id -1) * params["ngenes"] + 1 : min(target_id * params["ngenes"], params["nsamples"] * params["ngenes"])
+    Y_ = Y[target_range]
+    best_MSE = 10e8
+    for iter in 1:nminibatches
+        batch_range = (iter -1) * batchsize + 1 : min(iter * batchsize, params["nsamples"] * batchsize)
+        X_ = (X[1][batch_range],X[2][batch_range]) 
+        @time trained_FE.net(X_)
+        # @time MM = reshape(trained_FE.net(X_), (nsamples_batchsize, params["ngenes"]))
+        # diff_2_MM = vec(sum((MM .- Y_') .^ 2, dims = 2))
+        # diff_2_MM_min_val = minimum(diff_2_MM)
+        # diff_2_MM_min_index = (iter - 1) * nsamples_batchsize +  findfirst(diff_2_MM .== diff_2_MM_min_val)
+        
+        # if diff_2_MM_min_val <= best_MSE
+        #     new_embed[target_id, :] .= vec(cpu(trained_FE.net[1][1].weight[:,diff_2_MM_min_index]))
+        #     best_MSE = diff_2_MM_min_val
+        # end 
+    end 
+    target_id % 10 == 0 ? println("$target_id $(patients[target_id]) \t ") : nothing
+end 
+
+# implement patient embedding restart 
+# copy 
+infer_model = reset_embedding_layer_sample_init(trained_FE.net,params,params["nsamples"])
+
+# impute training set patient embed, compute MSE to target, take lowest 20%
+# fine-tune patient embed 2000 steps
+# 
+
+
+# trained_patient_FE = cpu(trained_FE.net[1][1].weight)
+trained_patient_FE = cpu(infer_model[1][1].weight)
 TCGA_colors_labels_df = CSV.read("Data/GDC_processed/TCGA_colors_def.txt", DataFrame)
 
 patient_embed_fig = plot_patient_embedding(trained_patient_FE, labs, "trained 2-d embedding\n$(params["modelid"])", params["colorsFile"]) 
 CairoMakie.save("$(params["outpath"])/$(params["modelid"])_2D_embedding_$(iter).png", patient_embed_fig)
 # inference         
+
+function train_restart_model_SGD_per_sample!(params, X, Y, model; printstep = 1_000)
+    start_timer = now()
+    nsamples_batchsize = params["nsamples_batchsize"]
+    batchsize = params["ngenes"] * nsamples_batchsize
+    nminibatches = Int(floor(params["nsamples"] / nsamples_batchsize))
+    tr_loss, tr_epochs, tr_cor, tr_elapsed = [], [], [], []
+    opt = Flux.ADAM(params["lr"])
+    println("1 epoch 1 - 1 /$nminibatches - TRAIN \t ELAPSED: $((now() - start_timer).value / 1000 )")         
+        
+    # shuffled_ids = collect(1:length(Y)) # very inefficient
+    for iter in 1:params["nsteps"]
+        # Stochastic gradient descent with minibatches
+        cursor = (iter -1)  % nminibatches + 1
+        # if cursor == 1 
+        #     shuffled_ids = shuffle(collect(1:length(Y))) # very inefficient
+        # end 
+        # id_range = (cursor -1) * batchsize + 1:min(cursor * batchsize, params["nsamples"])
+        # ids = shuffled_ids[id_range]
+        batch_range = (cursor -1) * batchsize + 1 : min(cursor * batchsize, params["nsamples"] * batchsize)
+        X_, Y_ = (X[1][batch_range],X[2][batch_range]), Y[batch_range]
+        ps = Flux.params(model[1][1])
+        # dump_cb(model, params, iter + restart)
+        gs = gradient(ps) do 
+            Flux.mse(model(X_), Y_) + params["l2"] * sum(p -> sum(abs2, p), ps) ## loss
+        end
+        lossval = Flux.mse(model(X_), Y_) + params["l2"] * sum(p -> sum(abs2, p), ps)
+        pearson = my_cor(model(X_), Y_)
+        Flux.update!(opt,ps, gs)
+        push!(tr_cor, pearson)
+        push!(tr_loss, lossval)
+        push!(tr_epochs, Int(ceil(iter / nminibatches)))
+        push!(tr_elapsed, (now() - start_timer).value / 1000 )
+        (iter % 100 == 0) | (iter == 1) ? println("$(iter) epoch $(Int(ceil(iter / nminibatches))) - $cursor /$nminibatches - TRAIN loss: $(lossval)\tpearson r: $pearson ELAPSED: $((now() - start_timer).value / 1000 )") : nothing        
+            
+        if (iter % printstep == 0) 
+            CSV.write("$(params["outpath"])/$(params["modelid"])_loss_computing_times", DataFrame(:tr_epochs=>tr_epochs, :tr_loss=>tr_loss, :tr_elapsed=>tr_elapsed))
+            # # save model 
+            # bson("$(params["outpath"])/$(params["modelid"])_restart_in_training_model.bson", Dict("model"=> cpu(model)))
+            trained_patient_FE = cpu(model[1][1].weight)
+            patient_embed_fig = plot_patient_embedding(trained_patient_FE, labs, "trained 2-d embedding\n$(params["modelid"]) \n- step $(iter)", params["colorsFile"]) 
+            CairoMakie.save("$(params["outpath"])/$(params["modelid"])_restart_2D_embedding_$(iter).png", patient_embed_fig)
+        end 
+    end
+    # save model 
+    # bson("$(params["outpath"])/$(params["modelid"])_model_$(params["nsteps"]).bson", Dict("model"=> cpu(model.net)))
+    return tr_epochs, tr_loss, tr_cor, tr_elapsed
+end
+
+train_restart_model_SGD_per_sample!(params, X, Y, infer_model)
+trained_patient_FE = cpu(infer_model[1][1].weight)
+TCGA_colors_labels_df = CSV.read("Data/GDC_processed/TCGA_colors_def.txt", DataFrame)
+
+patient_embed_fig = plot_patient_embedding(trained_patient_FE, labs, "trained 2-d embedding\n$(params["modelid"])", params["colorsFile"]) 
+
 #test_model = do_inference(trained_FE.net, params, test_data,  patients[test_ids], genes[CDS] )
 # inference with pre-training init embed  
 #test_model = do_inference(trained_FE.net, params, test_data,  patients[test_ids], genes[CDS], pre_trained_init = true)
