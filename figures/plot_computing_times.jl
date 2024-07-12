186f5441c9d7a5a7a77f8dcc9886f8e71b3a69f9
sauves <leonardsauve@gmail.com>
2024-07-11 20:51:56 -0400

    generated FE computing times by embsize figures

--- figures/plot_computing_times.jl
+++ figures/plot_computing_times.jl
@@ -2,6 +2,33 @@ include("engines/init.jl")
 include("engines/factorized_embeddings.jl")
 include("engines/figures.jl")
 include("engines/data_processing.jl")
+# collect computing times from selected runs
+df = gather_params("models")
+df = df[[!in(modelid, ["b8741c8c88e600acdad2b", "f18903a0c47b2a417f8fa", "4a7e104fa545eacceb473", "b8741c8c88e600acdad2b ", "933c5075d0a4a77fe08d5","f1ee9a10160c95c93555f", "c499367f5c8816182820a", "a849a50a8ed8add8d58f7", "d93427f8fb0a889d9efb4", "971790dffebf5bb40a370", "df4d540cccb4ad4c2c945"]) for modelid in df[:,"modelid"] ],:] 
+avgspeeds = []
+embsizes = []
+for row_id in 1:size(df)[1]
+    comp_times = CSV.read("$(df[row_id,"outpath"])/$(df[row_id,"modelid"])_loss_computing_times", DataFrame)
+    comp_times[:,"stepn"] .= collect(1:size(comp_times)[1])
+    comp_times[:, "speed"] .= 1 ./ (comp_times.tr_elapsed .- vcat(comp_times.tr_elapsed[1], comp_times.tr_elapsed[1:end-1]))
+    comp_times = comp_times[2:end,:]
+    push!(avgspeeds, mean(comp_times[:,"speed"]))
+    push!(embsizes, df[row_id,"emb_size_2"])
+    println("$(df[row_id,"modelid"])\t$(df[row_id,"emb_size_2"])\t", mean(comp_times[:,"speed"]))
+    
+end 
+to_plot = DataFrame(Dict("emb_size"=>embsizes, "speed"=>avgspeeds))
+emb_sizes_sorted = sort(unique(embsizes))
+means = [round(mean(to_plot[to_plot[:,"emb_size"] .== emb, "speed"]), digits = 2) for emb in  emb_sizes_sorted]
+posit = Dict([(embsize, i) for (i, embsize) in  enumerate(emb_sizes_sorted)])
+fig = Figure();
+ax = Axis(fig[1,1], title = "Average speed of training by number of nodes in the gene embedding layer", xlabel = "number of nodes in gene embedding layer", ylabel = "average speed (iteration/second)", xticks = (collect(1:length(posit)),string.(emb_sizes_sorted)) );
+boxplot!(ax, Int.([posit[emb] for emb in embsizes]), Float32.(avgspeeds), show_outliers = false)
+scatter!(ax,  Int.([posit[emb] for emb in embsizes]), Float32.(avgspeeds), color = :white, strokewidth=2)
+text!(ax, collect(1:length(emb_sizes_sorted)) .- 0.3, means .+ 1, text = string.(means))
+fig
+CairoMakie.save("figures/FE_computing_times_by_embedding_size.pdf", fig)
+CairoMakie.save("figures/FE_computing_times_by_embedding_size.png", fig)
 
 # merge computing times files 
 basepath = "interactive_figures/comp_times/"
--- runs/test_inference_1_0.jl
+++ runs/test_inference_1_0.jl
@@ -5,7 +5,7 @@ include("engines/data_processing.jl")
 include("engines/utils.jl")
 include("engines/gpu_utils.jl")
 include("engines/classification.jl")
-
+device!()
 outpath, session_id = set_dirs("FE_RES")
 TCGA_data, labs, patients, genes, biotypes = load_tcga_dataset("Data/GDC_processed/TCGA_TPM_lab.h5")
 labels = annotate_labels(labs, "Data/GDC_processed/TCGA_abbrev.txt")
@@ -17,8 +17,8 @@ nfolds = 5
 folds = split_train_test(X_data, nfolds = nfolds) # split 80-20
 ACCs = []
 modelid = "$(bytes2hex(sha256("$(now())"))[1:Int(floor(end/3))])"
-# outdir = "inference_1_0_FE_models"
-outdir = "tmp"
+outdir = "inference_1_0_FE_models"
+# outdir = "tmp"
 for foldn in 1:nfolds
     train_ids, train_data, test_ids, test_data = folds[foldn]["train_ids"], folds[foldn]["train_x"], folds[foldn]["test_ids"], folds[foldn]["test_x"]
     ## train for 80% 
@@ -26,15 +26,15 @@ for foldn in 1:nfolds
         ## run infos 
         "session_id" => session_id,  "modelid" =>  modelid,
         "outpath"=>outpath, "machine_id"=>strip(read(`hostname`, String)), "device" => "$(device())",
-        "printstep"=>1000, "foldn" => foldn,
+        "printstep"=>10000, "foldn" => foldn,
         "colorsFile"=> "Data/GDC_processed/TCGA_colors_def.txt",
         ## data infos 
         "nsamples" =>size(X_data)[1], "ngenes"=> size(X_data)[2],  
         "nsamples_batchsize"=> nsamples_batchsize, 
         ## optim infos 
-        "lr" => 1e-2, "l2" => 1e-8,"nsteps" => 100, "nsteps_inference" => 100, "batchsize" => nsamples_batchsize * size(X_data)[2],
+        "lr" => 1e-2, "l2" => 1e-8,"nsteps" => 100_000, "nsteps_inference" => 10_000, "batchsize" => nsamples_batchsize * size(X_data)[2],
         ## model infos
-        "emb_size_1" => 75, "emb_size_2" => 100, "fe_layers_size"=> [100,50,50]#, "fe_hl1_size" => 50, "fe_hl2_size" => 50,
+        "emb_size_1" => 3, "emb_size_2" => 100, "fe_layers_size"=> [100,50,50]#, "fe_hl1_size" => 50, "fe_hl2_size" => 50,
         )
     
     # train with training set
