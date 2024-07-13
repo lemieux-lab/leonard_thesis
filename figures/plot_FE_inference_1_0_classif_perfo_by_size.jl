69db426d1e3858727865dc66f2c4afa992b6179d
sauves <leonardsauve@gmail.com>
2024-07-13 11:51:29 -0400

    updated figure

--- figures/plot_FE_inference_1_0_classif_perfo_by_size.jl
+++ figures/plot_FE_inference_1_0_classif_perfo_by_size.jl
@@ -14,29 +14,39 @@ umap_results = gather_params("inference_UMAP_models")
 umap_mini_df = umap_results[:,["n_components", "foldn", "modelid","classif_ACC"]]
 umap_results = unique(umap_mini_df, view=true)
 
+pca_results = gather_params("inference_PCA_models")
+pca_mini_df = pca_results[:,["n_components", "foldn", "modelid","classif_ACC"]]
+pca_results = unique(pca_mini_df, view=true)
 
-emb_sizes_sorted = sort(unique(results[:, "emb_size_1"]))
-means = [round(median(results[results[:,"emb_size_1"] .== emb, "classif_ACC"]) * 100, digits = 1)  for emb in  emb_sizes_sorted]
+FE_sizes_sorted = sort(unique(results[:, "emb_size_1"]))
+FE_vals = [round(median(results[results[:,"emb_size_1"] .== emb, "classif_ACC"]) * 100, digits = 1)  for emb in  FE_sizes_sorted]
 
-comp_size_sorted = sort(unique(umap_results[:, "n_components"]))
-umap_vals = [round(median(umap_results[umap_results[:,"n_components"] .== emb, "classif_ACC"]) * 100, digits = 1)  for emb in  comp_size_sorted]
+UMAP_sizes_sorted = sort(unique(umap_results[:, "n_components"]))
+UMAP_vals = [round(median(umap_results[umap_results[:,"n_components"] .== emb, "classif_ACC"]) * 100, digits = 1)  for emb in  UMAP_sizes_sorted]
 
-posit = Dict([(embsize, i) for (i, embsize) in  enumerate(emb_sizes_sorted)])
-fig = Figure(size = (512,512));
+PCA_sizes_sorted = sort(unique(pca_results[:, "n_components"]))
+PCA_vals = [round(median(pca_results[pca_results[:,"n_components"] .== emb, "classif_ACC"]) * 100, digits = 1)  for emb in  PCA_sizes_sorted]
+
+posit = Dict([(embsize, i) for (i, embsize) in  enumerate(FE_sizes_sorted)])
+fig = Figure(size = (750,512));
 ax = Axis(fig[1,1], 
     title = "Average accuracy of cancer type prediction \nby number of nodes in the patient embedding layer",
-    xlabel = "number of nodes in patient embedding layer", 
+    xlabel = "Size of dimensionality of reduction", 
     ylabel = "Accuracy", 
     limits = (0, nothing, nothing, 100),
-    xticks = (collect(1:length(posit)),string.(emb_sizes_sorted)),
-    yticks = (collect(40:10:100),string.(collect(40:10:100))) );
+    xticks = (collect(1:length(posit)),string.(FE_sizes_sorted)),
+    yticks = (collect(30:10:100),string.(collect(30:10:100))) );
 boxplot!(ax, Int.([posit[emb] for emb in results[:,"emb_size_1"]]) .- 0.2, Float32.(results[:,"classif_ACC"]) * 100, width = 0.5, show_outliers = false, label = "FE")
 scatter!(ax,  Int.([posit[emb] for emb in results[:,"emb_size_1"]]) .- 0.2, Float32.(results[:,"classif_ACC"]) * 100, strokewidth=2)
-text!(ax, collect(1:length(emb_sizes_sorted)) .- 0.85, means .-1, text = string.(means), fontsize = 14)
+text!(ax, collect(1:length(FE_sizes_sorted)) .- 0.35, FE_vals .+ 5, text = string.(FE_vals), fontsize = 14)
 
 boxplot!(ax, Int.([posit[emb] for emb in umap_results[:,"n_components"]]) .+ 0.2, Float32.(umap_results[:,"classif_ACC"]) * 100,width = 0.5, show_outliers = false, label = "UMAP")
 scatter!(ax,  Int.([posit[emb] for emb in umap_results[:,"n_components"]]) .+ 0.2, Float32.(umap_results[:,"classif_ACC"]) * 100, strokewidth=2)
-text!(ax, collect(1:length(comp_size_sorted)), umap_vals .+ 2, text = string.(umap_vals), fontsize = 14)
+text!(ax, collect(1:length(UMAP_sizes_sorted)), UMAP_vals .+ 2, text = string.(UMAP_vals), fontsize = 14)
+
+boxplot!(ax, Int.([posit[emb] for emb in pca_results[:,"n_components"]]) .+ 0.2, Float32.(pca_results[:,"classif_ACC"]) * 100,width = 0.5, color = :magenta, show_outliers = false, label = "PCA")
+scatter!(ax,  Int.([posit[emb] for emb in pca_results[:,"n_components"]]) .+ 0.2, Float32.(pca_results[:,"classif_ACC"]) * 100, color = :magenta, strokewidth=2)
+text!(ax, collect(1:length(PCA_sizes_sorted)), PCA_vals .+ 2, text = string.(PCA_vals), fontsize = 14)
 
 axislegend(ax, position=:rb)
 fig
---
+++ runs/test_PCA.jl
@@ -0,0 +1,54 @@
+include("engines/init.jl")
+include("engines/figures.jl")
+include("engines/data_processing.jl")
+include("engines/classification.jl")
+include("engines/factorized_embeddings.jl")
+include("engines/pca.jl")
+outpath, session_id = set_dirs("UMAP_RES")
+TCGA_data, labs, patients, genes, biotypes = load_tcga_dataset("Data/GDC_processed/TCGA_TPM_lab.h5")
+CDS = biotypes .== "protein_coding"
+X_data = TCGA_data[:,CDS]
+nfolds = 5
+folds = split_train_test(X_data, nfolds = nfolds) # split 80-20
+ACCs = []
+modelid = "$(bytes2hex(sha256("$(now())"))[1:Int(floor(end/3))])"
+outdir = "inference_PCA_models"
+foldn = 1
+
+for foldn in 1:nfolds
+    train_ids, train_data, test_ids, test_data = folds[foldn]["train_ids"], folds[foldn]["train_x"], folds[foldn]["test_ids"], folds[foldn]["test_x"]
+    generate_pca_params(X_data; dim_redux_size = 2) = return Dict( 
+        ## run infos 
+        "session_id" => session_id,  "modelid" =>  modelid,
+        "outpath"=>outpath, "machine_id"=>strip(read(`hostname`, String)), "device" => "$(device())",
+        "printstep"=>10000, "foldn" => foldn, "modeltype"=>"PCA",
+        "colorsFile"=> "Data/GDC_processed/TCGA_colors_def.txt",
+        ## data infos 
+        "nsamples" =>size(X_data)[1], "ngenes"=> size(X_data)[2],  
+        ## optim infos 
+        
+        ## model infos
+        "n_components"=>dim_redux_size, 
+        )
+    params = generate_pca_params(train_data; dim_redux_size = 125)
+    start_timer = now()
+    train_PCA = fit_pca(train_data', params["n_components"])
+    elapsed = (now() - start_timer).value / 1000
+    train_PCA_x = transform_pca(train_data', train_PCA)
+    test_PCA_x = transform_pca(test_data', train_PCA)
+    ACC = test_performance_PCA(train_PCA_x, test_PCA_x, labs, train_ids, test_ids)
+
+    params["classif_ACC"] = ACC
+    params["total_time"] = elapsed
+    dump_patient_embedding_CSV(train_PCA_x, "$(foldn)_train_$(params["n_components"])D_PCA", params, labs, train_ids,outdir=outdir)
+    dump_patient_embedding_CSV(test_PCA_x, "$(foldn)_test_$(params["n_components"])D_PCA", params, labs, test_ids, outdir=outdir)
+    println("$(ACC)\tElapsed time: $(elapsed)")
+end 
+
+# ## plotting embed using UMAP 
+# fig = Figure(size = (512,512));
+# ax2 = Axis(fig[1,1], xlabel = "UMAP-1", ylabel="UMAP-2", aspect = 1);
+# for group_lab in unique(labs)
+#     group = labs .== group_lab
+#     plot!(umap_model.embedding[1,group],umap_model.embedding[2,group], label = group_lab)
+# end 
